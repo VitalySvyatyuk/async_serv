4e62cead3693a10b44b2433f17f12cb7f3b2cc16
Vitaly Svyatyuk <goodgame1945@gmail.com>
2016-12-16 14:06:16 +0300

    server is work sync

--- server.py
+++ server.py
@@ -19,7 +19,7 @@
 
 
 
-
+from urllib.parse import parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
  
 
@@ -39,6 +39,13 @@ class ServerRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
+        length = int(self.headers['Content-Length'])
+        post_data = parse_qs(self.rfile.read(length).decode('utf-8'))
+        try:
+            value = int(post_data['number'][0])
+        except:
+            value = "Введённое значение должно быть натуральным числом"
+
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
@@ -47,10 +54,9 @@ class ServerRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body><form action='http://127.0.0.1:8081' method='POST'>", "utf-8"))
         self.wfile.write(bytes("<input type='text' name='number'>", "utf-8"))
         self.wfile.write(bytes("<input type='submit' value='Разложить'></form>", "utf-8"))
-        self.wfile.write(bytes("<p>And you know what?.. This is POST!</p>", "utf-8"))
+        self.wfile.write(bytes("<p>{}</p>".format(factor(value)), "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
- 
 
 def run():
     print('starting server...')
@@ -59,17 +65,23 @@ def run():
     print('running server...')
     httpd.serve_forever()
 
-def factor(n):
-    Ans = []
-    d = 2
-    while d * d <= n:
-        if n % d == 0:
-            Ans.append(d)
-            n //= d
-        else:
-            d += 1
-    if n > 1:
-        Ans.append(n)
-    return Ans
+def factor(val):
+    if isinstance(val, int):
+        Ans = []
+        d = 2
+        n = val
+        while d * d <= n:
+            if n % d == 0:
+                Ans.append(d)
+                n //= d
+            else:
+                d += 1
+        if n > 1:
+            Ans.append(n)
+        multipliers = ""
+        for an in Ans:
+            multipliers += str(an) + " * "
+        val = (str(val) + " = " + multipliers).rstrip("* ")
+    return val
 
 run()
